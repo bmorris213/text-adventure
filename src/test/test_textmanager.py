47a68df2cedc50848343f52533657949165cb4c8
bmorris213 <thisisnotalreadyused@gmail.com>
2024-05-27 06:52:52 -0600

    writing and running unit tests for text manager

--- src/test/test_textmanager.py
+++ src/test/test_textmanager.py
@@ -3,19 +3,60 @@
 # Brian Morris
 
 import unittest
+from unittest.mock import patch, call
 
-from textmanager import TextManager
+import textmanager
 
 # Test TextManager
 # unit tests for text manager
 class TestTextManager(unittest.TestCase):
+    # ensure prompting user works as intended
+    @patch('builtins.print')
+    @patch('textmanager.time.sleep', return_value=None)
+    def test_display_text(self, mock_sleep, mock_print):
+        test_string = "Testing1...\nTesting2...\nTesting3..."
+        test_values = [ 0, textmanager.QUICK_TEXT, textmanager.NORMAL_TEXT, textmanager.SLOW_TEXT, 500 ]
+
+        for i in range(len(test_values)):
+            textmanager.display_text(test_string, test_values[i])
+        
+        # ensure correct breaking up of prints in print with 0 delay
+        mock_print.assert_any_call(f"{textmanager.TERMINAL_TAG}Testing1...")
+        mock_print.assert_any_call(f"{textmanager.TERMINAL_TAG}Testing2...")
+        mock_print.assert_any_call(f"{textmanager.TERMINAL_TAG}Testing3...")
+
+        # ensure delays have been called with the correct values
+        expected_calls = []
+        for i in range(len(test_values)):
+            if i != 0:
+                adjusted_time_delay = min(test_values[i], textmanager.MAX_DELAY)
+                char_delay = adjusted_time_delay / 5
+                line_delay = adjusted_time_delay * 5
+
+                for line in test_string.split('\n'):
+                    expected_calls.extend([call(char_delay) for _ in line])
+                    expected_calls.append(call(line_delay))
+
+        self.assertEqual(mock_sleep.call_count, len(expected_calls))
+        mock_sleep.assert_has_calls(expected_calls, any_order=True)
+
+
+    # ensure user input retrieval is handled successfully
+    def test_get_input(self):
+        test_suite = [ "test", " words with spaces ", "CAPS    WORDS", "something_invalid", "     " ]
+        results = [ "test", "words with spaces", "caps words", None, None ]
+
+        with patch('builtins.input', side_effect=test_suite) as mock_input:
+            for i in range(len(test_suite)):
+                self.assertEqual(textmanager.get_input(), results[i])
+
+    # ensure character cases are lowered safely
     def test_to_lower(self):
-        # define tests
-        test_suite = [ "test", "TEST", "T", "o", "Te1s1T!", [ '_', " Test ", '_' ] ]
+        test_suite = [ "test", "TEST", "T", "o", "Te1s1T!", [ "_", " Test ", "_" ] ]
         results = [ "test", "test", "t", "o", "te1s1t!", "_ test _" ]
 
         for i in range(len(test_suite)):
-            self.assertEqual(TextManager.to_lower(test_suite[i]), results[i])
+            self.assertEqual(textmanager.to_lower(test_suite[i]), results[i])
 
 if __name__ == '__main__':
     unittest.main()
--- src/textmanager.py
+++ src/textmanager.py
@@ -3,6 +3,7 @@
 # Brian Morris
 
 import time
+import sys
 
 ALPHABET = {
     'A' : 'a', 'B' : 'b', 'C' : 'c', 'D' : 'd', 'E' : 'e',
@@ -14,6 +15,7 @@ ALPHABET = {
 TERMINAL_TAG = "  *"
 PLAYER_TAG = " > "
 
+MAX_DELAY = .5
 SLOW_TEXT = .3
 NORMAL_TEXT = .15
 QUICK_TEXT = .05
@@ -21,93 +23,97 @@ QUICK_TEXT = .05
 # Text Manager
 # Performs operations on strings
 # Handles console i/o
-class TextManager():
-    # Display Text
-    # used to communicate information to the user in a common format
-    # animated with text_speed
-    def display_text(text, text_speed=0):
-        # break up display by newline characters
-        text_lines = text.split('\n')
-        
-        # print start of new terminal line
+
+# Display Text
+# used to communicate information to the user in a common format
+# animated with text_speed
+def display_text(text, text_speed=0):
+    # break up display by newline characters
+    text_lines = text.split('\n')
+
+    # print start of new terminal line
+    if text_speed != 0:
         print(TERMINAL_TAG, end='')
+    
+    if text_speed > MAX_DELAY:
+        text_speed = MAX_DELAY
 
-        for line in text_lines:
-            # print instantaneously with speed == 0
-            if text_speed == 0:
-                print(f"{line}")
+    for line in text_lines:
+        # print instantaneously with speed == 0
+        if text_speed == 0:
+            print(f"{TERMINAL_TAG}{line}")
+        else:
+            # delay between print operations
+            for letter in line:
+                time.sleep(text_speed / 5)
+                print(letter, end='', flush=True)
+            # end line
+            time.sleep(text_speed * 5)
+            print('', end='\n', flush=True)
+            if line != text_lines[len(text_lines) - 1]:
+                print(TERMINAL_TAG, end='', flush=True)
+
+# Get Input
+# ensures user input is of valid type: words with characters a-z seperated by spaces
+# returns None if invalid values are used
+def get_input():
+    # get arg_string from user
+    arg_string = input(f"\n{PLAYER_TAG}", file=sys.stdin)
+
+    result = ""
+    # skip multiple spaces in a row
+    space_found = False
+
+    for character in arg_string:
+        # skip all multiple spaces in a row
+        if space_found == True:
+            if character == ' ':
+                continue
             else:
-                # delay between print operations
-                for letter in line:
-                    time.sleep(text_speed / 5)
-                    print(letter, end='', flush=True)
-                # end line
-                time.sleep(text_speed * 5)
-                print('', end='\n', flush=True)
-                if line != text_lines[len(text_lines) - 1]:
-                    print(TERMINAL_TAG, end='', flush=True)
-
-    # Get Input
-    # ensures user input is of valid type: words with characters a-z seperated by spaces
-    # returns None if invalid values are used
-    def get_input():
-        # get arg_string from user
-        arg_string = input(f"\n{PLAYER_TAG}")
-
-        result = ""
-        # skip multiple spaces in a row
-        space_found = False
-
-        for character in arg_string:
-            # skip all multiple spaces in a row
-            if space_found == True:
-                if character == ' ':
-                    continue
-                else:
-                    space_found = False
+                space_found = False
             
-            if character >= 'a' and character <= 'z':
-                # a-z are valid input
-                result += character
-            elif character >= 'A' and character <= 'Z':
-                # A-Z need to become a-z
-                result += TextManager.to_lower(character)
-            elif character == ' ':
-                # first of potentially many spaces
-                result += ' '
-                space_found = True
-            else:
-                # invalid character returned
-                return None
-        
-        # strip ends of spaces
-        if len(result) != 0:
-            result = result.lstrip(' ')
-        if len(result) != 0:
-            result = result.rstrip(' ')
-        
-        # test for missing result
-        if len(result) == 0:
+        if character >= 'a' and character <= 'z':
+            # a-z are valid input
+            result += character
+        elif character >= 'A' and character <= 'Z':
+            # A-Z need to become a-z
+            result += to_lower(character)
+        elif character == ' ':
+            # first of potentially many spaces
+            result += ' '
+            space_found = True
+        else:
+            # invalid character returned
             return None
         
-        # whatever is left is a clean user input string
-        return result
+    # strip ends of spaces
+    if len(result) != 0:
+        result = result.lstrip(' ')
+    if len(result) != 0:
+        result = result.rstrip(' ')
+        
+    # test for missing result
+    if len(result) == 0:
+        return None
+        
+    # whatever is left is a clean user input string
+    return result
 
-    # To Lower
-    # Safely reduces A-Z to lower case
-    # returns unmodified if not A-Z
-    def to_lower(arg_string):
-        # perform recursive operation on objects of size > 1
-        if len(arg_string) != 1:
-            new_lower_input = ""
-            for character in arg_string:
-                new_lower_input += TextManager.to_lower(character)
-            return new_lower_input
+# To Lower
+# Safely reduces A-Z to lower case
+# returns unmodified if not A-Z
+def to_lower(arg_string):
+    # perform recursive operation on objects of size > 1
+    if len(arg_string) != 1:
+        new_lower_input = ""
+        for character in arg_string:
+            new_lower_input += to_lower(character)
+        return new_lower_input
         
-        # perform whichever operation is needed
-        for capital, lower_case in ALPHABET.items():
-            if arg_string == capital:
-                return lower_case
-        
-        # return unmodified value if no valid result was found
-        return arg_string
+    # perform whichever operation is needed
+    for capital, lower_case in ALPHABET.items():
+        if arg_string == capital:
+            return lower_case
+    
+    # return unmodified value if no valid result was found
+    return arg_string
